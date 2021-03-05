b76f98395cb2f09df912ae7000ebafcb87ea9601
alexlee-dev <alex@alexlee.dev>
2021-03-05 09:28:27 -0800

    Get List of Repos - #6

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,35 @@
 import * as React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect as BrowserRedirect,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { Octokit } from "@octokit/rest";
+import { response } from "express";
 
 const Home = () => {
+  const [accessToken, setAccessToken] = React.useState("");
+  const [repos, setRepos] = React.useState<any>([]);
+
+  React.useEffect(() => {
+    const localStorageAcessToken = window.localStorage.getItem("accessToken");
+    if (localStorageAcessToken) {
+      setAccessToken(localStorageAcessToken);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (accessToken) {
+      const octokit = new Octokit({ auth: accessToken });
+
+      octokit.repos
+        .listForAuthenticatedUser({ per_page: 100 })
+        .then((response) => setRepos(response.data))
+        .catch((error) => console.error(error));
+    }
+  }, [accessToken]);
+
   const handleIntegrateWithGitHub = async () => {
     const response = await fetch("/auth", {
       headers: {
@@ -22,15 +50,31 @@ const Home = () => {
   return (
     <div>
       <h1>repository-concierge</h1>
-      <button onClick={handleIntegrateWithGitHub}>Integrate with GitHub</button>
+      {!accessToken && (
+        <button onClick={handleIntegrateWithGitHub}>
+          Integrate with GitHub
+        </button>
+      )}
+      {repos && (
+        <ol>
+          {repos.map((repo: { id: React.Key; name: React.ReactNode }) => (
+            <li key={repo.id}>{repo.name}</li>
+          ))}
+        </ol>
+      )}
     </div>
   );
 };
 
 const Redirect = () => {
+  const [accessTokenSaved, setAccessTokenSaved] = React.useState(false);
   const accessToken = window.location.search.split("=")[1];
-  console.log({ accessToken });
-  return <h1>REDIRECT</h1>;
+  React.useEffect(() => {
+    window.localStorage.setItem("accessToken", accessToken);
+    setAccessTokenSaved(true);
+  }, [accessToken]);
+
+  return accessTokenSaved ? <BrowserRedirect to="/" /> : <h1>REDIRECT</h1>;
 };
 
 const App: React.FunctionComponent<unknown> = () => {
